68234f938b166489c202042ed0559f6419e58ad2
rflechner <rflechner@romcyber.com>
2018-04-05 22:29:24 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using ScrapySharp;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using static System.Console;
+
+namespace ScrapingExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var query = "scrapysharp";
+            WriteLine($"Searching '{query}' on google");
+
+            var browser = new ScrapingBrowser();
+            var resultsPage = await browser.NavigateToPageAsync(new Uri($"https://www.google.fr/search?q={query}"));
+            
+            WriteLine($"Results");
+
+            foreach (var link in resultsPage.Html.CssSelect("h3.r a"))
+            {
+                WriteLine($"- {link.InnerText}");
+            }
+
+        }
+    }
+}
